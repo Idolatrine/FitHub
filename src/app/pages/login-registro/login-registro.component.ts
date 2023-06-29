0bc9f7bebac0ecee2291c80be45054d9fc425170
Idolatrine <80734342+Idolatrine@users.noreply.github.com>
2023-06-29 08:57:02 -0400

    Add files via upload

--- src/app/pages/login-registro/login-registro.component.ts
+++ src/app/pages/login-registro/login-registro.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 
+import { AuthService } from "src/app/services/auth.service";
+
 @Component({
   selector: 'app-login-registro',
   templateUrl: './login-registro.component.html',
@@ -9,13 +11,15 @@ import {Router} from "@angular/router";
 })
 export class LoginRegistroComponent implements OnInit {
   formularioLogin!: FormGroup;
+  submitted=false;
 
-  constructor(private formBuilder: FormBuilder, private router: Router) {
+  constructor(private formBuilder: FormBuilder, private router: Router, private authService: AuthService) {
   }
 
   ngOnInit(): void {
+    
     let formulario = {
-      correo: ['', Validators.compose([
+      email: ['', Validators.compose([
         Validators.pattern(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/),
         Validators.required
       ])],
@@ -28,9 +32,24 @@ export class LoginRegistroComponent implements OnInit {
 
   }
   iniciarSesion() {
-    console.log(this.formularioLogin.status);
+    this.submitted=true
+   
+    if(this.formularioLogin.invalid){
+      return
+    }else{
+      alert("Bienvenido!!")
+    }
+
     if (this.formularioLogin.status === 'VALID') {
-      this.router.navigate([''])
+      this.router.navigate(['MiPerfil'])
     }
   }
+
+  login(): void {
+    this.authService
+      .login(this.formularioLogin.value.email, this.formularioLogin.value.password)
+      .subscribe();
+  }
+
 }
+
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, FormControl, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
+
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
   selector: 'app-registro',
@@ -9,21 +11,22 @@ import {Router} from "@angular/router";
 })
 export class RegistroComponent implements OnInit {
     formularioRegistro!: FormGroup;
-  constructor(private formBuilder: FormBuilder, private router: Router) {
+  constructor(private formBuilder: FormBuilder, private router: Router, private AuthService: AuthService) {
   }
 
   ngOnInit(): void {
     let formulario = {
-      nombreRealUsuario: ['', Validators.compose([
+      name: ['', Validators.compose([
           Validators.required
       ])],
-      nombreUsuario: ['', Validators.compose([
+      username: ['', Validators.compose([
           Validators.required
       ])],
-      edad: ['', Validators.compose([
+      age: ['', Validators.compose([
           Validators.pattern(/^(0?[1-9]|[1-9][0-9]|[1][1-9][1-9]|200)$/),
+          Validators.required
       ])],
-      correo: ['', Validators.compose([
+      email: ['', Validators.compose([
           Validators.pattern(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/),
           Validators.required
       ])],
@@ -38,8 +41,14 @@ export class RegistroComponent implements OnInit {
   registrarse(){
     console.log(this.formularioRegistro.status);
     if (this.formularioRegistro.status === 'VALID') {
-      this.router.navigate([''])
+      this.router.navigate(['Login'])
     }
   }
 
+  signup(): void {
+    this.AuthService.signup(this.formularioRegistro.value).subscribe((msg) => {
+      console.log(msg);
+      this.router.navigate(["Login"]);
+    });
+  }
 }
